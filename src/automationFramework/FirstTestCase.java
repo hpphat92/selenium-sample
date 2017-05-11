7139abb8691e9ef5d644604252faf974be16e2fc
Phat Hong <phat.hong@newoceaninfosys.com>
2017-05-11 13:08:56 +0700

    Add first test

--- src/automationFramework/FirstTestCase.java
+++ src/automationFramework/FirstTestCase.java
@@ -3,26 +3,34 @@ package automationFramework;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
 public class FirstTestCase {
 
-	public static void main(String[] args) throws InterruptedException {
-		// TODO Auto-generated method stub
-		
-		// Create a new instance of the Firefox driver
-		WebDriver driver = new FirefoxDriver();
-		
+    public static void main(String[] args){
+
+    }
+
+    @Test(priority = 1)
+    public void Test1() throws Exception {
+        // TODO Auto-generated method stub
+
+        // Create a new instance of the Firefox driver
+        WebDriver driver = new FirefoxDriver();
+
         //Launch the Online Store Website
-		driver.get("http://www.store.demoqa.com");
- 
+        driver.get("http://www.store.demoqa.com");
+
         // Print a Log In message to the screen
         System.out.println("Successfully opened the website www.Store.Demoqa.com");
- 
 
-		Thread.sleep(15);
-		
+
+        Thread.sleep(15);
+
         // Close the driver
         driver.quit();
-
-	}
-
+    }
 }
